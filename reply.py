04a2b2f2cc4e5dbb140faa16ff0e9a1d0fe3dda7
Asuto Katayama <y210023@mail.ryukoku.ac.jp>
2024-10-31 09:35:26 +0900

    2024.10.31

--- reply.py
+++ reply.py
@@ -15,17 +15,15 @@ import sqlite3
 
 
 time_short = 7
-time_long = 15
+time_long = 30
 menu_message = """
 記録の確認、タスクの登録、削除などを行いたい場合は以下を入力してください。
 
-[7分]         : 7分以内の合計を確認
-[15分]        : 15分以内の合計を確認
-[合計]        : 合計を確認
 [タスク登録]  : タスクを登録
 [タスク一覧]  : タスク一覧を表示
 [タスク削除]  : タスクを削除
 [タスク報告]  : タスクの実行を報告
+[記録確認]    : 記録を確認
 """
 default_message = '習慣化サポートボットです。以下でメニューを表示します。'
 
@@ -140,11 +138,12 @@ def response():
             # ユーザーからのメッセージを取得
             user_message = posted_object['events'][0]['message']['text']
             text = ""  # 返信テキスト
-            # 現在のタスクの状態を確認
+            # 現在のタスクの状態を確認,task_idがnoneでない場合
             current_task_id = Message.query.filter_by(user_id=user_id_from_line).order_by(Message.time.desc()).first()
             if current_task_id:
                 print('current_task_id:', current_task_id.task_id)
                 print(task_status[user_id_from_line])
+
             if user_message == 'キャンセル':
                 if task_status[user_id_from_line] == 'waiting_for_task_goal':
                     #タスクの登録をキャンセル
@@ -154,26 +153,11 @@ def response():
                 task_status[user_id_from_line] = 'idle'
                 text = 'キャンセルしました。\n'+'[メニュー] : メニューを表示'
                 Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
+
             else:
                 if task_status[user_id_from_line] == 'idle':  # 通常の状態
-                    if user_message == '7分':
-                        #timeが7分以内のもののnumberの合計を取得
-                        seven_minutes_ago = datetime.datetime.now() - datetime.timedelta(minutes=time_short)
-                        total = db.session.query(func.sum(Message.number)).filter(Message.time >= seven_minutes_ago, Message.user_id == posted_object['events'][0]['source']['userId']).scalar()
-                        text = '7分以内の合計は、' + str(total) + 'です。'
-                        Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
-                    elif user_message == '15分':
-                        #timeが30分以内のもののnumberの合計を取得
-                        thirty_days_ago = datetime.datetime.now() - datetime.timedelta(minutes=time_long)
-                        total = db.session.query(func.sum(Message.number)).filter(Message.time >= thirty_days_ago, Message.user_id == posted_object['events'][0]['source']['userId']).scalar()
-                        text = '15分以内の合計は、' + str(total) + 'です。'
-                        Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
-                    elif user_message == '合計':
-                        #numberの合計を取得ただし、userが1のものだけ
-                        total = db.session.query(func.sum(Message.number)).filter(Message.user_id == posted_object['events'][0]['source']['userId']).scalar()
-                        text = '合計は、' + str(total) + 'です。'
-                        Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
-                    elif user_message == 'タスク登録':
+
+                    if user_message == 'タスク登録':
                         text = 'タスクを登録します。タスク名を入力してください。\n\n'+'現在のタスク一覧\n'
                         tasks = Task.query.filter_by(user_id=user_id_from_line, enable=True).all()
                         for task in tasks:
@@ -181,6 +165,7 @@ def response():
                         text += 'キャンセルする場合は「キャンセル」と入力してください。'
                         task_status[user_id_from_line] = 'waiting_for_task_name'
                         Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
+
                     elif user_message == 'タスク一覧':
                         #タスク一覧を取得
                         tasks = Task.query.filter_by(user_id=user_id_from_line, enable=True).all()
@@ -191,6 +176,7 @@ def response():
                             for task in tasks:
                                 text += f'{task.task_name}: {task.daily_goal}\n'
                         Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
+
                     elif user_message == 'タスク削除':
                         #タスク削除
                         tasks = Task.query.filter_by(user_id=user_id_from_line, enable=True).all()
@@ -203,6 +189,7 @@ def response():
                             text += 'キャンセルする場合は「キャンセル」と入力してください。'
                             task_status[user_id_from_line] = 'waiting_for_task_delete'
                         Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
+
                     elif user_message == 'タスク報告':
                         #タスクの記録
                         tasks = Task.query.filter_by(user_id=user_id_from_line, enable=True).all()
@@ -215,13 +202,28 @@ def response():
                             text += 'キャンセルする場合は「キャンセル」と入力してください。'
                             task_status[user_id_from_line] = 'waiting_for_task_record'
                         Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
+
                     elif user_message == 'メニュー':
                         text = menu_message
                         Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
+
+                    elif user_message == '記録確認':
+                        tasks = Task.query.filter_by(user_id=user_id_from_line, enable=True).all()
+                        if not tasks:
+                            text = '登録されているタスクがありません。'
+                        else:
+                            text = '今までの記録を確認します。記録を確認したいタスク名を入力してください。\n'+'現在のタスク一覧\n'
+                            for task in tasks:
+                                text += f'{task.task_name}: {task.daily_goal}\n'
+                            text += 'キャンセルする場合は「キャンセル」と入力してください。'
+                            task_status[user_id_from_line] = 'waiting_for_confilm_name'
+                        Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
+
                     else:
                         #text = get_reply(user_message)+'\n'+'[メニュー] : メニューを表示'
                         text = default_message+'\n'+'[メニュー] : メニューを表示'
                         Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
+                        
                 elif task_status[user_id_from_line] == 'waiting_for_task_name':
                     #同じタスク名が登録されていないか確認
                     current_task_name = Task.query.filter_by(user_id=user_id_from_line, task_name=user_message, enable=True).first()
@@ -237,6 +239,7 @@ def response():
                         db.session.add(new)
                         db.session.commit()
                         Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'], task_id = new.task_id)
+
                 elif task_status[user_id_from_line] == 'waiting_for_task_goal':
                     # タスクの目標数を受け取る
                     current_task = Task.query.filter_by(task_id=current_task_id.task_id ,user_id=user_id_from_line, enable=True).first()
@@ -251,18 +254,39 @@ def response():
                     except ValueError:
                         text = '目標数は整数で入力してください。'
                     Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'], task_id = current_task.task_id)
+
                 elif task_status[user_id_from_line] == 'waiting_for_task_delete':
                     # タスクの削除
                     # タスク名が登録されているか確認
                     current_task_delete_name = Task.query.filter_by(user_id=user_id_from_line, task_name=user_message, enable=True).first()
                     if current_task_delete_name:
+                        text = f"タスク '{user_message}' を削除します。本当に削除しますか？\n[はい]、[いいえ]のいずれかを入力してください。"
+                        task_status[user_id_from_line] = 'waiting_for_task_delete_confirm'
+                        Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'], task_id = current_task_delete_name.task_id)
+                        """
                         current_task_delete_name.enable = False
                         db.session.commit()
                         text = f"タスク '{user_message}' を削除しました。"
+                        """
                     else:
                         text = f"タスク '{user_message}' は登録されていません。\n"+'[メニュー] : メニューを表示'
+                        task_status[user_id_from_line] = 'idle'
+                        Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
+
+                elif task_status[user_id_from_line] == 'waiting_for_task_delete_confirm':
+                    # タスクの削除
+                    current_task_delete_name = Task.query.filter_by(task_id = current_task_id.task_id, user_id=user_id_from_line, enable=True).first()
+                    if user_message == 'はい':
+                        current_task_delete_name.enable = False
+                        db.session.commit()
+                        text = f"タスク '{current_task_delete_name.task_name}' を削除しました。"
+                    elif user_message == 'いいえ':
+                        text = f"タスク '{current_task_delete_name.task_name}' の削除をキャンセルしました。"
+                    else:
+                        text = '「はい」、「いいえ」のいずれかを入力してください。'
                     task_status[user_id_from_line] = 'idle'
-                    Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'], task_id = current_task_delete_name.task_id)
+                    Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'], task_id = current_task_delete_name.task_id)                        
+
                 elif task_status[user_id_from_line] == 'waiting_for_task_record':
                     # タスクの記録
                     current_task = Task.query.filter_by(user_id=user_id_from_line, task_name=user_message, enable=True).first()
@@ -274,6 +298,7 @@ def response():
                         text = f"タスク '{user_message}' は登録されていません。\n"+'[メニュー] : メニューを表示'
                         task_status[user_id_from_line] = 'idle'
                     Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'], task_id = current_task.task_id)
+
                 elif task_status[user_id_from_line] == 'waiting_for_task_record_number':
                     # タスクの記録
                     current_task = Task.query.filter_by(task_id = current_task_id.task_id, user_id=user_id_from_line, enable=True).first()
@@ -281,6 +306,7 @@ def response():
                         number = int(user_message)
                         Message04 = Message(content=user_message, time=datetime.datetime.now(), number = number, user_id = posted_object['events'][0]['source']['userId'], task_id = current_task.task_id)
                         text = f"タスク '{current_task.task_name}' の実行数 {number} を記録しました。"
+                        current_task.time = datetime.datetime.now()
                         if number >= current_task.daily_goal:
                             text += '目標達成おめでとうございます！'
                         else:
@@ -289,6 +315,38 @@ def response():
                         text = '実行数は整数で入力してください。'
                         Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
                     task_status[user_id_from_line] = 'idle'
+
+                elif task_status[user_id_from_line] == 'waiting_for_confilm_name':
+                    # タスクの記録
+                    current_task = Task.query.filter_by(user_id=user_id_from_line, task_name=user_message, enable=True).first()
+                    print('current_task:', current_task.task_id)
+                    if current_task:
+                        text = f"タスク '{user_message}' の記録を確認します。今までの記録を確認したい日付を入力してください。\n[合計]、[7日]、[30日]のいずれかを入力してください。"
+                        task_status[user_id_from_line] = 'waiting_for_confilm_date'
+                        Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'], task_id = current_task.task_id)
+                    else:
+                        text = f"タスク '{user_message}' は登録されていません。\n"+'[メニュー] : メニューを表示'
+                        task_status[user_id_from_line] = 'idle'
+                        Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'])
+
+                elif task_status[user_id_from_line] == 'waiting_for_confilm_date':
+                    # タスクの記録
+                    current_task = Task.query.filter_by(task_id = current_task_id.task_id, user_id=user_id_from_line, enable=True).first()
+                    if user_message == '合計':
+                        total = db.session.query(func.sum(Message.number)).filter(Message.user_id == posted_object['events'][0]['source']['userId'], Message.task_id == current_task.task_id).scalar()
+                        text = f"タスク '{current_task.task_name}' の合計は {total} です。"
+                    elif user_message == f'{time_short}日':
+                        seven_days_ago = datetime.datetime.now() - datetime.timedelta(days=time_short)
+                        total = db.session.query(func.sum(Message.number)).filter(Message.time >= seven_days_ago, Message.user_id == posted_object['events'][0]['source']['userId'], Message.task_id == current_task.task_id).scalar()
+                        text = f"タスク '{current_task.task_name}' の{time_short}日間の合計は {total} です。"
+                    elif user_message == f'{time_long}日':
+                        thirty_days_ago = datetime.datetime.now() - datetime.timedelta(days=time_long)
+                        total = db.session.query(func.sum(Message.number)).filter(Message.time >= thirty_days_ago, Message.user_id == posted_object['events'][0]['source']['userId'], Message.task_id == current_task.task_id).scalar()
+                        text = f"タスク '{current_task.task_name}' の{time_long}日間の合計は {total} です。"
+                    else:
+                        text = '日付は「合計」、「7日」、「30日」のいずれかを入力してください。'
+                    task_status[user_id_from_line] = 'idle'
+                    Message04 = Message(content=user_message, time=datetime.datetime.now(), number = 0, user_id = posted_object['events'][0]['source']['userId'], task_id = current_task.task_id)
         db.session.add(Message04)
         db.session.commit()
         print('登録 =>', Message04)
