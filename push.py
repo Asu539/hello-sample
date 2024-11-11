1f1fa8b18426274f398002be78eb9edc9cb7b1a4
Asuto Katayama <y210023@mail.ryukoku.ac.jp>
2024-11-11 09:03:08 +0900

    kansei

--- push.py
+++ push.py
@@ -6,6 +6,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, select, func
 import sqlite3
 import os
+import datetime
+import lineconfig
+
 
 # インスタンス生成
 app = Flask(__name__)
@@ -24,36 +27,79 @@ db = SQLAlchemy(app)
 connection = sqlite3.connect("line_data.sqlite", check_same_thread=False)
 cursor = connection.cursor()
 
-# ユーザーモデル
-class User(db.Model):
-    user_id = db.Column(db.String(200), primary_key=True, unique=True, nullable=False)
 
 # モデル
 class Message(db.Model):
-    __tablename__ = 'talks'
+    __tablename__ = 'Messages'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     content = db.Column(db.String(200), nullable=False)
     time = db.Column(db.DateTime, nullable=False)
-    #ユーザーかそれ以外かを0か1で表す
-    #user = db.Column(db.Integer, nullable=False, default=0)
     number = db.Column(db.Integer, nullable=False, default=0)
     user_id = db.Column(db.String(200), nullable=False)
+    task_id = db.Column(db.Integer, autoincrement=True)
+    status = db.Column(db.String(200), nullable=False, default='idle')
 
+#タスクモデル
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    task_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    user_id = db.Column(db.String(200), nullable=False)
+    task_name = db.Column(db.String(200), nullable=False, default='未定')
+    daily_goal = db.Column(db.Integer, nullable=False, default=0)
+    is_done = db.Column(db.Boolean, nullable=False, default=0) #タスク実行したかどうか
+    enable = db.Column(db.Boolean, nullable=False, default=True)
+    report = db.Column(db.Integer, nullable=False, default=0)
+
+# DB作成
+def init_db():
+    with app.app_context():
+        db.create_all()
 
 headers={
    'Content-Type': 'application/json',
    'Authorization': "Bearer "+lineconfig.CHANNELACCESSTOKEN
 }
+#rows = cursor.execute('SELECT DISTINCT user_id FROM tasks').fetchall()
+#user_ids = [row[0] for row in rows]
 
-payload={
-    'to':'U4b4ca417d98f6d17369aa34174839f1c',
-    'messages':[
-        {
-        'type':'text',
-        'text':'日本語も通る?'
-        }
-    ]
-}
-response = requests.post(lineconfig.PUSHAPIURL, data=json.dumps(payload),headers=headers)
-print(response.status_code) 
-print(response.text) 
+# メイン処理
+with app.app_context():
+    #タスクテーブルのuser_idを被り無しで取得
+    user_ids = db.session.query(Task.user_id).distinct().all()
+    user_ids = [user_id[0] for user_id in user_ids]     #タプルを解除してリストに
+    if len(user_ids) == 0:
+        print('ユーザーがいません')
+        exit()
+    else:
+        for user_id_from_line in user_ids:
+            #task.do_taskが0のものを取得
+            tasks = Task.query.filter(Task.user_id == user_id_from_line, Task.enable == True ,Task.is_done == 0).all()
+            print(user_id_from_line)
+            if tasks == []:
+                text = '未実行タスクはありません\n明日も頑張りましょう！'
+            else:
+                text = '未実行タスクがあります\n\n未実行タスク一覧\n'
+                for task in tasks:
+                    text += f'{task.task_name}: {task.daily_goal}\n'
+                text += 'タスクを実行しましょう！'
+                print(text)
+            payload={
+                    'to':user_id_from_line,
+                    'messages':[
+                        {
+                        'type':'text',
+                        'text':text
+                        }
+                    ]
+                }
+            #全てのtask.do_taskを0にする
+            tasks = Task.query.filter(Task.user_id == user_id_from_line, Task.is_done == 1).all()
+            for task in tasks:
+                task.is_done = 0
+            Message05 = Message(content=text, time=datetime.datetime.now(), number = 0, user_id = 'line')            
+            db.session.add(Message05)
+            db.session.commit()
+            response = requests.post(lineconfig.PUSHAPIURL, data=json.dumps(payload),headers=headers)
+            print(response.status_code) 
+            print(response.text) 
